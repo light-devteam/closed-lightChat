4c2c6733e053c752d6cab921dadf1257c0c626e6
rTiRe <61313696+rTiRe@users.noreply.github.com>
2022-05-26 19:54:09 +0300

    Delete lightChatSpigot directory

--- lightChatSpigot/src/main/java/tire/lightchat/modules/Random.java
+++
@@ -1,9 +0,0 @@
-package tire.lightchat.modules;
-
-public class Random {
-    public static int rnd(int min, int max)
-    {
-        max -= min;
-        return (int) (Math.random() * ++max) + min;
-    }
-}
